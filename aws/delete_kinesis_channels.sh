115b4aa47220ef1c730c2fa0ed747aea165c3b98
mcrivar <eddie.nazarov@verbit.ai>
2022-09-13 10:34:24 +0300

    add spacing

--- aws/delete_kinesis_channels.sh
+++ aws/delete_kinesis_channels.sh
@@ -5,8 +5,10 @@ REGION=$1
 if [ -z $REGION ]; then echo "Usage: $(basename "$0") <REGION>" && exit 0; fi
 
 channels=$(aws kinesisvideo list-signaling-channels --region eu-west-1 | jq -r '.[][].ChannelARN')
+
 echo "Total channels to delete:"
 echo $(aws kinesisvideo list-signaling-channels --region eu-west-1 | jq -r '.[][].ChannelARN' | wc -l)
+
 STATUS=0
 for channel in ${channels[@]}; do
 	aws kinesisvideo delete-signaling-channel --channel-arn $channel
